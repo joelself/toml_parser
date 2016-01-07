1e07733269dcd39fb7c9f55258aa4add0ab7b22f
Joel Self <joel@dislocal.com>
2016-01-06 21:32:05 -0700

    Added tests dir. Started toml asset file test methods.

---
+++ tests/assets.rs
@@ -0,0 +1,31 @@
+extern crate tomllib;
+use std::fs;
+use std::fs::DirEntry;
+
+fn process_toml_file(file: &DirEntry) -> Option<&str> {
+	// TODO: Read file => parse file => write file => compare file to original
+	return Option::None;
+}
+
+#[test]
+fn test_all_assets() {
+	let paths = fs::read_dir("./assets/").unwrap();
+    let mut failed: Vec<&str> = vec![];
+
+    for path in paths {
+    	let file = path.unwrap();
+    	if file.file_type().unwrap().is_file() {
+    		let path = &*file.path();
+    		match path.extension() {
+    			Some(ext) if ext.to_str() == Some("toml") => {
+    				match process_toml_file(&file) {
+    					Some(ref name) 	=> failed.push(name),
+    					_	 			=> {}
+    				};
+    			},
+    			_ => {} 
+    		};
+    	}
+    }
+    // TODO: if failed is non-empty assert false with all the names of failed files
+}
